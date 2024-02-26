e232ec0afdc8360b598495864cb59a6715bc042d
coin0527 <nako2020@naver.com>
2024-02-26 15:02:19 +0900

    .

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -6,6 +6,27 @@ const Wrap = styled.div`
   background-color: lightgray;
 `;
 
+const Box1 = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  height: 450px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 150px 5%;
+`;
+
 export const Section1 = () => {
-  return <Wrap> 섹션1 </Wrap>;
+  return (
+    <Wrap>
+      <Container>
+        <Box1 />
+        <Box1 />
+        <Box1 />
+      </Container>
+    </Wrap>
+  );
 };
