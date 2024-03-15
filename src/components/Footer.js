2b34bafccfa71f961bffd57af421751ad4033969
coin0527 <nako2020@naver.com>
2024-03-15 22:19:36 +0900

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,110 +1,41 @@
-import {
-  faInstagram,
-  faXTwitter,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-const Wrap = styled.div`
+const Wrap = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 200px;
+  border: 1px solid black;
   display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
-
-const Bars = styled.div`
-  width: 100%;
-  height: 50px;
-  background-color: white;
-`;
-
-const Text = styled.div`
-  width: 100%;
-  height: 500px;
-  background-color: lightskyblue;
-  text-align: center;
-  padding: 80px;
-  white-space: pre-line;
-  text-shadow: 2px 4px silver;
-
-  p1 {
-    font-size: 50px;
-    font-weight: bold;
-    color: snow;
-  }
-  p2 {
-    font-size: 60px;
-    font-weight: bold;
-    color: snow;
-  }
-`;
-
-const Line = styled.div`
-  width: 50%;
-  border: 5px solid snow;
-  border-radius: 10px 10%;
+const Logo = styled.div`
+  width: 30%;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 30px auto;
-  text-align: center;
 `;
-
-const Sns = styled.div`
+const Bar = styled.div`
+  border: 1px solid black;
+`;
+const Introduce = styled.div`
   width: 100%;
-  max-width: 30%;
   display: flex;
-  justify-content: space-between;
-  margin-top: 50px;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
+  justify-content: center;
+  align-items: center;
 `;
-
-const open = (url) => {
-  window.open(url, "_blank");
-};
 
 export const Footer = () => {
   return (
     <Wrap>
-      <Bars />
-      <Text>
-        <p1>
-          ☆彡
-          <br />
-          Starlight Garden
-        </p1>
-        <p2>
-          <br />
-          OFFICAL SNS
-        </p2>
-        <Line />
-        <Sns>
-          <div
-            onClick={() => open("https://www.instagram.com/allday_0709/?hl=ko")}
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              style={{ fontSize: "80px", color: "white" }}
-            />
-          </div>
-          <div
-            onClick={() => open("https://www.instagram.com/allday_0709/?hl=ko")}
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              style={{ fontSize: "80px", color: "snow" }}
-            />
-          </div>
-          <div onClick={() => open("/")}>
-            <FontAwesomeIcon
-              icon={faYoutube}
-              style={{ fontSize: "80px", color: "snow" }}
-            />
-          </div>
-        </Sns>
-      </Text>
+      <Container>
+        <Logo>123</Logo>
+        <Bar />
+        <Introduce>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
+          officia, libero cumque natus nobis nam voluptatibus quam unde
+          perspiciatis ab quasi blanditiis voluptatem, aspernatur consectetur
+          possimus provident inventore perferendis temporibus?
+        </Introduce>
+      </Container>
     </Wrap>
   );
 };
