5aa2b81d4a9e0d1d826e4fc08edfbe5262e9c263
coin0527 <nako2020@naver.com>
2024-03-26 13:33:23 +0900

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,20 @@
 import styled from "styled-components";
 
-const Wrap = styled.div``;
-const Container = styled.div`
+const Wrap = styled.div`
   width: 100%;
-  height: 200px;
-  border: 1px solid black;
-  display: flex;
-`;
-const Logo = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const Bar = styled.div`
-  border: 1px solid black;
-`;
-const Introduce = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Con = styled.div`
-  width: 300px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
-const Imagebox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 30px;
+  h3 {
+    color: #333;
+    display: flex;
+    justify-content: center;
+    line-height: 30px;
+  }
 `;
 
 export const Footer = () => {
   return (
     <Wrap>
-      <Container>
-        <Con>
-          <Logo>logo</Logo>
-          <Imagebox>Image</Imagebox>
-        </Con>
-        <Bar />
-        <Introduce>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
-          officia, libero cumque natus nobis nam voluptatibus quam unde
-        </Introduce>
-      </Container>
+      <h3> &copy; 2024. JTube. All rights reserved. </h3>
     </Wrap>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Header } from "./Header";
 import { Section1 } from "./Section1";
+import { Footer } from "./Footer";
 
 const Wrap = styled.div`
   width: 100%;
@@ -12,6 +13,7 @@ export const Home = () => {
     <Wrap>
       <Header />
       <Section1 />
+      <Footer />
     </Wrap>
   );
 };
